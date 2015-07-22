76f8eb0ee6654beac3c716e5ddf0c50573fde92a
Francis Chartrand <chartrandf@bonyweb.com>
2015-07-21 20:14:41 -0400

    02 command line bin from swift

---
+++ 02-hello-world/Hello World/Hello World/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Hello World
+//
+//  Created by Francis Chartrand on 2015-07-21.
+//  Copyright (c) 2015 Francis Chartrand. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
